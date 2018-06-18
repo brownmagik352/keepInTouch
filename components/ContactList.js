5c5b8045c7686bb131fbd99ed481655c80c32afa
Apurv Suman <sumanicus@gmail.com>
2018-06-18 07:13:33 -0700

    toggle contact list items

--- components/ContactList.js
+++ components/ContactList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, FlatList, StyleSheet, Text } from 'react-native';
+import { View, FlatList, StyleSheet, Text, Button } from 'react-native';
 import ContactListItem from './ContactListItem';
 import ActionableContact from './ActionableContact';
 
@@ -7,7 +7,8 @@ export default class ContactList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            chosen: []
+            chosen: [],
+            contactListItemsVisible: false
         }; 
     }
 
@@ -15,6 +16,14 @@ export default class ContactList extends Component {
         const chosenNames = this.state.chosen.slice();
         chosenNames.push({key: name});
         this.setState({chosen: chosenNames});
+    }
+
+    toggleContactListVisible() {
+        if (this.state.contactListItemsVisible) {
+            this.setState({contactListItemsVisible: false});
+        } else {
+            this.setState({contactListItemsVisible: true});
+        }
     }
 
     render() {
@@ -26,22 +35,26 @@ export default class ContactList extends Component {
                     ListEmptyComponent={<Text>No Reminders Set</Text>}
                 />
 
-                <FlatList
-                data={[
-                    {key: 'Devin'},
-                    {key: 'Jackson'},
-                    {key: 'James'},
-                    {key: 'Joel'},
-                    {key: 'John'},
-                    {key: 'Jillian'},
-                    {key: 'Jimmy'},
-                    {key: 'Julie'},
-                ]}
-                renderItem={({item}) => <ContactListItem 
-                                            name={item.key} 
-                                            onPress={() => this.handlePress(item.key)}
-                                        />}
-                />
+                <Button title="Show/Hide Contacts" onPress={() => this.toggleContactListVisible()} />
+
+                { this.state.contactListItemsVisible && 
+                    <FlatList
+                    data={[
+                        {key: 'Devin'},
+                        {key: 'Jackson'},
+                        {key: 'James'},
+                        {key: 'Joel'},
+                        {key: 'John'},
+                        {key: 'Jillian'},
+                        {key: 'Jimmy'},
+                        {key: 'Julie'},
+                    ]}
+                    renderItem={({item}) => <ContactListItem 
+                                                name={item.key} 
+                                                onPress={() => this.handlePress(item.key)}
+                                            />}
+                    />
+                }
             </View>
         )
     }
